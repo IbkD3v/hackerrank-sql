f65959e10270f12185202301bdb15d0e6af0b56b
IbkD3v <42724152+IbkD3v@users.noreply.github.com>
2023-02-12 04:01:16 +0100

    Update SelectByID.sql

--- Revision/SelectByID.sql
+++ Revision/SelectByID.sql
@@ -1,1 +1,19 @@
+--Query all columns for all American cities in CITY with populations larger than 100,000. The CountryCode for America is USA.
 
+--Input Format
+
+--The CITY table is described as follows:
+
+
+/*
+Field	 Type
+ID   	 NUMBER
+NAME	 VARCHAR2(17)
+COUNTRY CODE	VARCHAR2(3)
+DISTRICT	VARCHAR2(20)
+POPULATION	NUMBER */
+
+
+SOLUTION-
+
+SELECT * FROM CITY WHERE ID ='1661';
